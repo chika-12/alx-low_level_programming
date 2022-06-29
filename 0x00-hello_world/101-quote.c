a086aa631d385e29c2aae62e06c4ca9db4ac2eba
chika-12 <markworship001@gmail.com>
2022-06-29 14:47:16 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-1  #include <unistd.h>
-2  /**
-3   * main - Entry point
-4   *
-5   * Return: Always 1 (Success)
-6   */
-7 int main(void)
-8 {
-9          char str1[] = "and the piece of art is useful\" - Dora korpar, 2015-10-19\n";
-10
-11         write(2, str1, 59);
-12         return (1);
-13}
+ #include <unistd.h>
+ /**
+  * main - Entry point
+  *
+  * Return: Always 1 (Success)
+  */
+int main(void)
+{
+         char str1[] = "and the piece of art is useful\" - Dora korpar, 2015-10-19\n";
+
+         write(2, str1, 59);
+         return (1);
+}
