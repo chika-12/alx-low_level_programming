77011589598f8e7bbb8cf18d9c451fd07df104f3
chika-12 <markworship001@gmail.com>
2022-06-30 06:47:36 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #incliude<stdio.h>
-   /**
-    *main - Entry point
-    *
-    * Return:Always 0  (success)
-    */	
+  
+
 int main(0)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
