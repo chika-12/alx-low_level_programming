8cb29c5eee3ea878ecc403eb72a21a153e55ed54
chika-12 <markworship001@gmail.com>
2022-07-01 11:53:36 -0700

    Print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - print the alphabet.
  *
@@ -8,8 +8,8 @@ int main(void)
 {
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; i<26; i++)
-{	
+for (i = 0; i < 26; i++)
+{
 putchar(alp[i]);
 }
 putchar('\n');
