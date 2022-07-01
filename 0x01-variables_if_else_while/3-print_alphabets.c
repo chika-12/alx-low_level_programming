77ada70ec7377e4c8ac4826a0c3b8fa3e7b065cd
chika-12 <markworship001@gmail.com>
2022-07-01 12:17:16 -0700

    Print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,12 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 int i;
 for (i = 0; i < 52; i++)
 {
+putchar(vialp[i]);
+}
 putchar('\n');
 return (0);
 }
