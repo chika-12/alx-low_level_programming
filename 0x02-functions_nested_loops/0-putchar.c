61e4be4598087f0d9625552bf1421451efa98fbc
chika-12 <markworship001@gmail.com>
2022-07-05 16:14:18 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
-#include "manin.h"
+#include "main.h"
 /**
  * main - print putchar
  * Return: Always 0
  */
-void print_putchar(void);
+int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
