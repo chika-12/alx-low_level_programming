1d8ce4233e4a42635ed7a4b7da89bb568489638f
chika-12 <markworship001@gmail.com>
2022-07-05 17:49:41 -0700

    Print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_X10 -> prints the lowercase alphabets
+ * print_alphabet_x10 -> prints the lowercase alphabets
  */
-void print_alphabet_X10(void)
+void print_alphabet_x10(void)
 {
 int j;
 int X;
