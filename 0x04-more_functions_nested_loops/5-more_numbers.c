0a2bf4b407649aec0e4baeff1ff0343520176c4f
chika-12 <markworship001@gmail.com>
2022-07-07 15:42:58 -0700

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,16 @@
  */
 void more_numbers(void)
 {
-int x, y;
-for (x = 10; x < 10; x++)
-{
-for (y = 0; y < 15; y++)
-{
-if (y >= 10)
-_putchar((y / 10) + '0');
-_putchar((y % 10) + '0');
-}
-_putchar('\n');
-}
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			}
+		_putchar('\n');
+	}
 }
