4083982b81ea8cfe4216c0b03ecc7e92ffe67f06
chika-12 <markworship001@gmail.com>
2022-07-12 03:15:50 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,18 +5,18 @@
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 int main(void)
 {
 	int n;
 
 	n = 402;
-	{
-	printf("n=%d\n",n);
-	}
+
+	printf("n=%d\n", n);
+
 	*p = 98;
-	{
-	printf("n=%d\n",n);
-	}
+
+	printf("n=%d\n", n);
+
 	return (0);
 }
