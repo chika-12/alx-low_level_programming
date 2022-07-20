2e290b50d9d4b0f4d179c1faa58c692e91bf30eb
chika-12 <markworship001@gmail.com>
2022-07-20 07:25:30 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * puts a function - a function that prints a string
+ * n@ parameters, str, char
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	char str[] = "stream";
+
+	printf("%s", str);
+	return (0);
+}
