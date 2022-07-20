19d0b0e505447eec1fe29bb867e5bcd3b944bbd2
chika-12 <markworship001@gmail.com>
2022-07-20 07:39:41 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts_recursion - prints a string followed by a new line.
  * @s: string
  * Return: no return
  */
 void _puts_recursion(char *s)
-{	
+{
 
 	if (*s != '\0')
 	{
