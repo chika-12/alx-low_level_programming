88f08479d06bfa8f536de0dc3d2cb4a0b3e21581
chika-12 <markworship001@gmail.com>
2023-12-14 16:23:36 +0100

    C_code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Understanding the use of write
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora kopar, 2015-10-19\n",58);
+	return (1);
+}
