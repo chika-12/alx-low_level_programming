1734d59b6adeeb05bd94aafb92fa3d48893cc27c
chika-12 <markworship001@gmail.com>
2023-12-15 17:18:57 +0100

    Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints both uppercase
+ * and lowercse alphabets
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar(10);
+}
