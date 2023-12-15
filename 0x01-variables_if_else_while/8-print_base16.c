c2600ff7ec171fbe843e9f1878797c44fdc3ee3d
chika-12 <markworship001@gmail.com>
2023-12-15 19:38:12 +0100

    Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal numbers
+ *
+ * Return: Always 0(Success)
+ *
+ */
+int main(void)
+{
+	int num = 48;
+	char alp;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	for (alp = 'a'; alp <= 'f'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar(10);
+}
