bae8776cd6698cbd08562f5a6cf2ea1663b4e8db
chika-12 <markworship001@gmail.com>
2023-12-15 17:04:37 +0100

    Variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet is lower case
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	return (0);
+}
