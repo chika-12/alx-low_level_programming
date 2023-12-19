1b8602ff429e13d23a948e783e3f82fa5e98c8d8
chika-12 <markworship001@gmail.com>
2023-12-19 13:08:16 +0100

    function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute value
+ * of a number
+ *
+ * @n: parameter
+ *
+ * Return: (0);
+ */
+
+int _abs(int n)
+{
+	int count = 0;
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			count += 1;
+		}
+	}
+	else if (n < 0)
+	{
+		for (i = n; i < 0; i++)
+		{
+			count += 1;
+		}
+	}
+	else
+	{
+		count = 0;
+	}
+	return (count);
+}
