a2effb5d95dd01fa01380f99692a344d267b1819
chika-12 <markworship001@gmail.com>
2023-12-21 16:09:39 +0100

    function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 1 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num;
+	int index = 0;
+
+	while (index < 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + 48);
+				_putchar((num % 10) + 48);
+			}
+			else
+			{
+				_putchar(num + 48);
+			}
+
+		}
+		_putchar(10);
+		index++;
+	}
+}
