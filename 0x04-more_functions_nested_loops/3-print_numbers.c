f4358f4465d36cd8f68d95cf0fc1751302b07bb1
chika-12 <markworship001@gmail.com>
2023-12-21 15:32:33 +0100

    function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from
+ * 1 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int index = 0;
+
+	while (index < 10)
+	{
+		_putchar(index + '0');
+		index++;
+	}
+}
