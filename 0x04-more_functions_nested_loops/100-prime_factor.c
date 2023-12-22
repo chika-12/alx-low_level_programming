62d3376e74d0c7e96a6c396c59eff2d3399e59e7
chika-12 <markworship001@gmail.com>
2023-12-22 09:40:23 +0100

    function

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * main - prints the largest prime number
+ * of 612852475143.
+ * Return: integer
+ */
+int main(void)
+{
+	long int number = 612852475143;
+	int index;
+	int new_idx;
+	int prime;
+
+	for (index = 2; index <= number; index++)
+	{
+		if (number % index == 0)
+		{
+			for (new_idx = 2; new_idx < index; new_idx++)
+			{
+				if (index % new_idx == 0)
+				{
+					break;
+				}
+			}
+			if (new_idx == index)
+			{
+				prime = index;
+				while (number % index == 0)
+				{
+					number /= index;
+				}
+			}
+		}
+	}
+	printf("Largest prime is %d\n", prime);
+	return (prime);
+}
