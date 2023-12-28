6eba28ddfe377cf9ecd38f3b7612f26ef68647c2
chika-12 <markworship001@gmail.com>
2023-12-28 03:03:31 +0100

    Add hello.c

---
+++ testing/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if a filename is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+# Set the path to your Git repository
+repo_path="/alx-low_level_programming/testing"
+
+# Navigate to the Git repository
+#cd "$repo_path" || exit
+
+# Git commands
+git add .
+git commit -m "Add $1"
+
+# Set up Git credentials
+git_credentials() {
+    #echo "Enter your Git username:"
+    #read -r username
+    #echo "Enter your Git password or access token:"
+    #read -r -s password
+   #echo
+}
+
+git_credentials
+
+# Run git push with credentials
+git push origin master
+
+# Clean up credentials
+unset username
+unset password
+
