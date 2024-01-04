6842ced0d5373f29ee9bbb4fbca181791025ad12
chika-12 <markworship001@gmail.com>
2024-01-04 22:05:59 +0100

    more pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: parameter
+ * @s2: parameter
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+	int flag = 0;
+
+	for (index = 0; s1[index] != '\0' && s2[index] != '\0'; index++)
+	{
+		if (s1[index] != s2[index])
+		{
+			if (s1[index] > s2[index])
+			{
+				flag = 1;
+			}
+			else if (s1[index] < s2[index])
+			{
+				flag = -1;
+			}
+			break;
+		}
+	}
+
+	return (flag);
+}
