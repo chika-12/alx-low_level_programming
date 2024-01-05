27ff57adc86320513b78835f9e05a3d50aa7a5a2
chika-12 <markworship001@gmail.com>
2024-01-05 20:05:26 +0100

    more pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,20 +7,20 @@
  */
 void print_number(int n)
 {
-		unsigned int new_num;
+	unsigned int new_num;
 
-		if (n  < 0)
-		{
-			new_num = -n;
-			_putchar('-');
-		}
-		else
-		{
-			new_num = n;
-		}
-		if (new_num / 10)
-		{
-			print_number(new_num / 10);
-		}
-		_putchar((new_num % 10) + 48);
+	if (n < 0)
+	{
+		new_num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		new_num = n;
+	}
+	if (new_num / 10)
+	{
+		print_number(new_num / 10);
+	}
+	_putchar((new_num % 10) + 48);
 }
