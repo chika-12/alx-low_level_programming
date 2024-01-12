46a0f5596b75b869b992a9184b6ae59dd7b6d832
chika-12 <markworship001@gmail.com>
2024-01-12 16:01:04 +0100

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - A program that prints its name
+ * @argc: argument to main
+ * @argv: commandline arrays passed to function
+ * Return: integer
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the implementation of argv and argc in c
