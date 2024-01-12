7e9634e6ff55838bd072e09e2f73d53f0c415682
chika-12 <markworship001@gmail.com>
2024-01-12 16:24:53 +0100

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - print all command line arguments recieved
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int index;
+
+	if (argc > 1)
+	{
+		for (index = 0; index < argc; index++)
+		{
+			printf("%s\n", argv[index]);
+		}
+	}
+	return (0);
+}
