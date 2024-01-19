ed9d32817894859df7c39452a2d1314ff6066123
chika-12 <markworship001@gmail.com>
2024-01-20 00:09:01 +0100

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
  * _calloc - allocates memory for an array
- * @nmemb: number of element
- * @size: size of element
+ * @arr: number of element
+ * @b: size of element
+ * @num: number of byte
  * Return: memory allocated
  */
+char *_memset(char *arr, char b, unsigned int num);
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr = NULL;
-	unsigned int index;
+
+	__attribute__((unused)) unsigned int index;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -19,10 +22,24 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-
-	for (index = 0; index < nmemb; index++)
-	{
-		ptr[index] = 0;
-	}
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
+}
+/**
+ * _memset - fills the specified memory location
+ * with specified chars to a specified number of byte
+ * @arr: memory location
+ * @b: character
+ * @num: number of byte
+ * Return: char
+ */
+char *_memset(char *arr, char b, unsigned int num)
+{
+	unsigned int index;
+
+	for (index = 0; index < num; index++)
+	{
+		arr[index] = b;
+	}
+	return (arr);
 }
