abf3edb463bfcd6a4b8b26f54a386af8d0e7f7e5
chika-12 <markworship001@gmail.com>
2024-01-19 22:53:16 +0100

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@ unsigned int _strlen(char *str);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr = NULL;
-	unsigned int index,len,total_len;
+	unsigned int index, len, total_len;
 
 	if (s1 == NULL)
 	{
