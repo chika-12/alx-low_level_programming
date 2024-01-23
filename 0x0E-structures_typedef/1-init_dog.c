e8c49522c265ab27cc91beb56474c1e2f849e362
chika-12 <markworship001@gmail.com>
2024-01-23 01:24:11 +0100

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a dog
+ * @d: parameter
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: struct dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
