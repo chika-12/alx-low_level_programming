cbdc7a9915acfd81124f31e6a12d2b332739cbaf
chika-12 <markworship001@gmail.com>
2024-01-23 02:36:16 +0100

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * dog_t - defines a new dog
+ */
+typedef struct dog_t;
+/**
  * struct dog - Defines a dog
  * @name: parameter
  * @age: parameter
@@ -13,7 +17,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
