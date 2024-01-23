f878b389a0ae31a4ebd965b313d359f7d1c84fdb
chika-12 <markworship001@gmail.com>
2024-01-23 23:53:24 +0100

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,25 +10,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *fresh_dog;
+	int len_name = 0, len_owner = 0, index;
 
 	fresh_dog = malloc(sizeof(dog_t));
 	if (fresh_dog == NULL)
 	{
 		return (NULL);
 	}
-	fresh_dog->age = age;
-	fresh_dog->name = strdup(name);
-	if (fresh_dog->name == NULL)
+	for (index = 0; name[index] != '\0'; index++)
 	{
+		len_name += 1;
+	}
+	for (index = 0; owner[index] != '\0'; index++)
+	{
+		len_owner += 1;
+	}
+	fresh_dog->name = malloc(sizeof(len_name + 1));
+	fresh_dog->owner = malloc(sizeof(len_owner + 1));
+	if (fresh_dog->name == NULL || fresh_dog->owner == NULL)
+	{
+		free(fresh_dog->name);
+		free(fresh_dog->owner);
 		free(fresh_dog);
 		return (NULL);
 	}
-	fresh_dog->owner = strdup(owner);
-	if (fresh_dog->owner == NULL)
+	fresh_dog->age = age;
+	for (index = 0; name[index] != '\0'; index++)
 	{
-		free(fresh_dog->name);
-		free(fresh_dog);
-		return (NULL);
+		fresh_dog->name[index] = name[index];
+	}
+	for (index = 0; owner[index] != '\0'; index++)
+	{
+		fresh_dog->owner[index] = owner[index];
 	}
 	return (fresh_dog);
 }
