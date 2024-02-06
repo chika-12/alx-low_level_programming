c890b9fa2118c771f67e629c05d2e9e44877af3a
chika-12 <markworship001@gmail.com>
2024-02-06 16:18:33 +0100

    linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * freelistint2 - frees a list.
+ * @head: list to be freed
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *node;
+
+	if (*head == NULL || head == NULL)
+		return;
+
+	while (ptr != NULL)
+	{
+		node = ptr->next;
+		free(ptr);
+		ptr = node;
+	}
+	*head = NULL;
+}
