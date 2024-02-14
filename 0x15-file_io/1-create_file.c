8fad28c8ac3910625f1fddf235f97bba7e2daf00
chika-12 <markworship001@gmail.com>
2024-02-14 03:42:44 +0100

    bytes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,5 +11,32 @@ int create_file(const char *filename, char *text_content)
 	char *buffer;
 	ssize_t written;
 
-
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	buffer = malloc(sizeof(char) * (strlen(text_content) + 1));
+	if (buffer == NULL)
+	{
+		return (-1);
+	}
+	strcpy(buffer, text_content);
+	written = write(file, buffer, strlen(buffer));
+	if (written == -1)
+	{
+		free(buffer);
+		return (-1);
+	}
+	free(buffer);
+	close(file);
+	return (1);
 }
