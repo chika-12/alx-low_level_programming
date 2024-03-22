4e79a31ce1405c7d0adefb029a82682d0b08cfa4
chika-12 <markworship001@gmail.com>
2024-03-22 05:19:26 +0100

    code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,12 +6,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 	const dlistint_t *ptr;
 
 	if (h == NULL)
 	{
-		return (EXIT_FAILURE);
+		return (count);
 	}
 	ptr = h;
 
