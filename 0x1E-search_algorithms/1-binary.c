23e1b31b4f16ddb94fff774e9de2e7e89731009a
chika-12 <markworship001@gmail.com>
2024-07-18 01:30:18 +0100

    Binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+/**
+ * binary_search - sorts and search
+ * @array: Array to be searched
+ * @size: Size of array
+ * @value: Value searched for
+ * Return: integer
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0;
+	size_t end = size - 1;
+	size_t index;
+
+	while (start <= end)
+	{
+		size_t mid = start + (end - start) / 2;
+
+		printf("Searching in array:");
+		for (index = start; index <= end; index++)
+		{
+			printf("%d", array[index]);
+			if (index < end)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (array[mid] < value)
+		{
+			start = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			end = mid - 1;
+		}
+		else
+		{
+			return (mid);
+		}
+	}
+	return (-1);
+
+}
